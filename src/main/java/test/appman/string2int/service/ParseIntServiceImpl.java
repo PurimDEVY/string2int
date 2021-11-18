fb945c1451a4c21e0f0506ae08d146d953c6d1da
PurimDEVY <cd212343@gmail.com>
2021-11-18 16:41:56 +0700

    parseint-by-arithmetic

--- src/main/java/test/appman/string2int/service/ParseIntServiceImpl.java
+++ src/main/java/test/appman/string2int/service/ParseIntServiceImpl.java
@@ -5,6 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import test.appman.string2int.model.IntServiceResponse;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 @Service
 public class ParseIntServiceImpl implements ParseIntService{
 
@@ -13,9 +17,38 @@ public class ParseIntServiceImpl implements ParseIntService{
     @Override
     public IntServiceResponse parseInt(String input) {
 
-        String StringReplace = input.replaceAll("[^0-9]", "");
-        LOG.info("String Replace: " + StringReplace);
-        IntServiceResponse intServiceResponse = new IntServiceResponse(StringReplace);
+        int num = 0;
+        int degit = 0;
+        List integers = Arrays.asList('0','1','2','3','4','5','6','7','8','9');
+        HashMap<Character,Integer> hashCharToInt = new HashMap<Character,Integer>();
+        hashCharToInt.put('0',0);
+        hashCharToInt.put('1',1);
+        hashCharToInt.put('2',2);
+        hashCharToInt.put('3',3);
+        hashCharToInt.put('4',4);
+        hashCharToInt.put('5',5);
+        hashCharToInt.put('6',6);
+        hashCharToInt.put('7',7);
+        hashCharToInt.put('8',8);
+        hashCharToInt.put('9',9);
+
+
+
+//        irritating backward from string input
+        for (int i = input.length()-1; i >= 0; i--) {
+            char letter = input.charAt(i);
+//            check character is integer
+            if(integers.contains(letter)){
+//                calculate integer eg. 123 = 1*100 + 2*10 + 3*1
+                int pow = 1;
+                for (int j = 0; j < degit; j++) {
+                     pow *= 10;
+                }
+                num += pow * hashCharToInt.get(letter);
+                degit++;
+            }
+        }
+        IntServiceResponse intServiceResponse = new IntServiceResponse(num);
         return intServiceResponse;
     }
 }
